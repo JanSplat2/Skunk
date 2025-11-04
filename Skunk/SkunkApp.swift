99564415bc9dec49ffd813e938bfcb54c0275199
JanSplat2 <jan.splat@outlook.de>
2025-11-04 14:07:26 -0700

    Initial Commit

---
+++ Skunk/SkunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkunkApp.swift
+//  Skunk
+//
+//  Created by Dittrich, Jan - Student on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SkunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
